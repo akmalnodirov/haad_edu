1312290ca9ae1e83dc3d079674853a884135fed7
Sardor Bozorov <bozorovsardor0610@gmail.com>
2023-12-22 00:41:13 -0800

    IGenericRepository has been created

--- HaadEdu.Domain/Repositories/IGenericRepository.cs
+++ HaadEdu.Domain/Repositories/IGenericRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace HaadEdu.Domain.Repositories;
+﻿using HaadEdu.Domain.Entities;
+using System.Linq.Expressions;
 
-public interface IGenericRepository
+namespace HaadEdu.Domain.Repositories;
+
+public interface IGenericRepository<T> where T : BaseEntity
 {
-    
+    public Task<T> CreateAsync(T entity);
+    public Task<bool> DeleteAsync(Expression<Func<T, bool>> expression);
+    public Task<T> UpdateAsync(T entity);
+    public Task<T> GetAsync(Expression<Func<T, bool>> expression, string[] include = null);
+    public IQueryable<T> GetAll(Expression<Func<T, bool>> expression,
+        string[] include = null,
+        bool isTracking = true);
+    public Task SaveChangesAsync();
 }
