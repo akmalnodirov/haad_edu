923a2cace7474eee0335abf44800a8a463748fec
Fazliddin <Fazliddin@DESKTOP-9RQEV5Q>
2023-12-29 11:32:08 +0500

    PasswordHashing and UserPasswordAttribute has been created.

---
+++ HaadEdu.Application/Attributes/UserPasswordAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HaadEdu.Application.Attributes
+{
+    public class UserPasswordAttribute : ValidationAttribute
+    {
+       
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (value is string password && password.Length >= 8 && password.Any(c => char.IsDigit(c))
+                    && password.Any(c => char.IsLetter(c))) ;
+                {
+                    return ValidationResult.Success;
+                }
+
+                return new ValidationResult("Sizning parolingiz yaroqli emas");
+            }
+        }
+    }
+}
